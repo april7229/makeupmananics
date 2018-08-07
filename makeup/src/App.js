0d461964b9ed0bf676795ee243fe9dcdce727905
april <martinezapril56@yahoo.com>
2018-08-07 01:47:10 -0400

    commit

--- makeup/src/App.js
+++ makeup/src/App.js
@@ -4,39 +4,64 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      
-        <div className = "header">
-        
-          <div className="container-fluid"></div>
-          <div>className = "container"></div>
-          <div className="container-fluid sticky-top"></div>
-          <div className = "container"></div>
-          <div className = "d-flex justify-content-end"></div>
-          <div className=" navbar navbar-expand-lg navbar-light bg-light"></div>
-          
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li classNamae="navbar-nav">
-                <a className="nav-item active">Home</a>
+    return <div className="header">
+        <div className="container-fluid" />
+        <div className="container" />
+        <div className="container-fluid sticky-top" />
+        <div className="container" />
+        <div className="d-flex justify-content-end" />
+        <div className=" navbar navbar-expand-lg navbar-light bg-light" />
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li classNamae="navbar-nav">
+              <a className="nav-item" href="#">
+                Home Page
+              </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">About Us</a>
+              <a className="nav-link" href="#">
+                About Us
+              </a>
             </li>
-            <li className="nav-item" >
-              <a className="nav-link" href="#"> Product Line</a>
-              </li>
-
-            </ul>
-          </div>
-          </div>
-         
-    
-  
-        
-          
-     
-    );
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                About Us
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                What's New?!
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Product Line
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Contact Us
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Shopping Cart
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                UpComing Events
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                UpComing Products
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>;
   }
 }
 
