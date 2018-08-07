552e3e0036de724e23844cd94154784544da1036
april <martinezapril56@yahoo.com>
2018-08-07 01:31:38 -0400

    commit

--- makeup/src/App.js
+++ makeup/src/App.js
@@ -5,15 +5,37 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      
+        <div className = "header">
+        
+          <div className="container-fluid"></div>
+          <div>className = "container"></div>
+          <div className="container-fluid sticky-top"></div>
+          <div className = "container"></div>
+          <div className = "d-flex justify-content-end"></div>
+          <div className=" navbar navbar-expand-lg navbar-light bg-light"></div>
+          
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li classNamae="navbar-nav">
+                <a className="nav-item active">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">About Us</a>
+            </li>
+            <li className="nav-item" >
+              <a className="nav-link" href="#"> Product Line</a>
+              </li>
+
+            </ul>
+          </div>
+          </div>
+         
+    
+  
+        
+          
+     
     );
   }
 }
